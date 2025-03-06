f5431dafbd80dc0fc50bac5e41fcac66a20d0f29
EvanCasey13 <evancasey2003@gmail.com>
2025-03-06 13:11:37 +0000

    Script to update python client automatically

--- generate_python_grpc_client.sh
+++ generate_python_grpc_client.sh
@@ -1,69 +1,19 @@
 #!/bin/bash
 
-set -e
+echo "Creating and activating the virtual environment"
+python3 -m venv venv
+source venv/bin/activate
 
-GITHUB_REPO_URL="https://github.com/project-kessel/relations-api"
-PROTO_FOLDER="api/kessel/relations/v1beta1"
-TEMP_DIR=$(mktemp -d)
-VENV_DIR="$TEMP_DIR/venv"
-OUTPUT_DIR=$(pwd)
+echo "Installing the relations api python client"
+python3 -m pip install project-kessel-relations-api-grpc-python --extra-index-url https://buf.build/gen/python
 
-function cleanup {
-  echo "Cleaning up temporary files..."
-  rm -rf "$TEMP_DIR"
-  echo "Done."
-}
+echo "Copying files to necessary directory"
+mkdir -p src/kessel/relations/v1beta1
+cp -r venv/lib/python*/site-packages/kessel/relations/v1beta1/* src/kessel/relations/v1beta1/
 
-trap cleanup EXIT
-
-cd "$TEMP_DIR"
-
-echo "Initializing an empty repository and setting sparse-checkout for $PROTO_FOLDER"
-git init
-git remote add origin "$GITHUB_REPO_URL"
-git config core.sparseCheckout true
-
-echo "$PROTO_FOLDER/*" >> .git/info/sparse-checkout
-echo "third_party/*" >> .git/info/sparse-checkout
-
-git pull origin main
-
-if command -v python3 &>/dev/null; then
-  PYTHON_CMD=python3
-elif command -v python &>/dev/null; then
-  PYTHON_CMD=python
-else
-  echo "Python is not installed. Please install Python and try again."
-  exit 1
-fi
-
-echo "Using Python command: $PYTHON_CMD"
-
-echo "Creating Python virtual environment..."
-$PYTHON_CMD -m venv "$VENV_DIR"
-
-source "$VENV_DIR/bin/activate"
-
-echo "Installing required packages..."
-pip install grpcio-tools
-
-echo "Generating Python client from proto files..."
-
-$PYTHON_CMD -m grpc_tools.protoc \
-            -I"$TEMP_DIR/third_party/"  \
-            -Ikessel/relations/v1beta1="$TEMP_DIR/$PROTO_FOLDER"  \
-            --python_out="$OUTPUT_DIR"/src \
-            --pyi_out="$OUTPUT_DIR"/src \
-            --grpc_python_out="$OUTPUT_DIR"/src \
-            "$TEMP_DIR/$PROTO_FOLDER"/*.proto \
-            "$TEMP_DIR/third_party/validate/"validate.proto
-
-echo "Python client generated in $TEMP_DIR"
-
-echo "Deactivating and removing the virtual environment..."
+echo "Deactivating the virtual environment"
 deactivate
-rm -rf "$VENV_DIR"
+echo "Removing the virtual environment"
+rm -rf "venv"
 
-find "$TEMP_DIR" -name "*.py"
-
-echo "Script completed successfully."
+echo "python client successfully updated"
