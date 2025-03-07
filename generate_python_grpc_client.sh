063c9da8f9c321defa18906b1a2c9c0eebe2fea9
EvanCasey13 <evancasey2003@gmail.com>
2025-03-07 13:25:45 +0000

    update client script to include new file path for validate files

--- generate_python_grpc_client.sh
+++ generate_python_grpc_client.sh
@@ -7,6 +7,10 @@ source venv/bin/activate
 echo "Installing the relations api python client"
 python3 -m pip install project-kessel-relations-api-grpc-python --extra-index-url https://buf.build/gen/python
 
+echo "Copying files to necessary directory for buf validation files"
+mkdir -p src/buf/validate
+cp -r venv/lib/python*/site-packages/buf/validate/* src/buf/validate
+
 echo "Copying files to necessary directory"
 mkdir -p src/kessel/relations/v1beta1
 cp -r venv/lib/python*/site-packages/kessel/relations/v1beta1/* src/kessel/relations/v1beta1/
